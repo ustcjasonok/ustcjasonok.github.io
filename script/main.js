9bb644b1884d40f7bb5b5bf3c1cfb3ffddd29726
Brody <1192280904@qq.com>
2021-04-11 00:45:22 +0800

    test

--- script/main.js
+++ script/main.js
@@ -106,12 +106,12 @@ const animationTimeline = () => {
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
-      scale: 1.2,
-      x: 10,
-      backgroundColor: "rgb(21, 161, 237)",
-      color: "#fff"
-    })
+    // .to(".idea-3 strong", 0.5, {
+    //   scale: 1.2,
+    //   x: 10,
+    //   backgroundColor: "rgb(21, 161, 237)",
+    //   color: "#fff"
+    // })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
